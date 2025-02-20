845795f7ea3910c7f3fb6c6058851f83e9b3a071
Ahmed Nasser <ahmednasser233005@gmail.com>
2025-02-21 00:03:36 +0200

    Add ShoppingCart

--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -1,43 +1,76 @@
 ﻿using BazzarBook.DataAccess.Repository.IRepository;
 using BazzarBook.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BazzarBookWeb.Areas.Customer.Controllers
 {
-    [Area("Customer")]
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IUnitOfWork _unitOfWork;
+	[Area("Customer")]
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
-        {
-            _logger = logger;
-            _unitOfWork = unitOfWork;
-        }
+		public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+		{
+			_logger = logger;
+			_unitOfWork = unitOfWork;
+		}
 
-        public IActionResult Index()
-        {
-            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
-            return View(productList);
-        }
+		public IActionResult Index()
+		{
+			IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+			return View(productList);
+		}
 
-        public IActionResult Details(int productId)
-        {
-            Product product = _unitOfWork.Product.Get(u=>u.Id== productId, includeProperties: "Category");
-            return View(product);
-        }
+		public IActionResult Details(int productId)
+		{
+			ShoppingCart shoppingCart = new()
+			{
+				Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+				Count = 1,
+				ProductId = productId
+			};
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+			return View(shoppingCart);
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[HttpPost]
+		[Authorize]
+		public IActionResult Details(ShoppingCart shoppingCart)
+		{
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+			shoppingCart.ApplicationUserId = userId;
+
+			ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId && u.ProductId == shoppingCart.ProductId);
+
+			if (cartFromDb != null)
+			{
+				cartFromDb.Count += shoppingCart.Count;
+				_unitOfWork.ShoppingCart.Update(cartFromDb);
+			}
+			else
+			{
+				_unitOfWork.ShoppingCart.Add(shoppingCart);
+			}
+
+			_unitOfWork.Save();
+
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
