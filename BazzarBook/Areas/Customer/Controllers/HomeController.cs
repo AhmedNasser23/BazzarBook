9a267f2161d64737e71a47add3af2c9689746a65
ahmad <ahmednasser2330052gmail.com>
2025-05-11 22:15:30 +0300

    Finish Home and Details Pages

--- BazzarBook/Areas/Customer/Controllers/HomeController.cs
+++ BazzarBook/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Bazzar.Models;
+using Bazzar.DataAccess.Repository.IRepository;
 
 namespace BazzarBook.Areas.Customer.Controllers
 {
@@ -8,15 +9,24 @@ namespace BazzarBook.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.ProductRepository.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.ProductRepository.Get(u => u.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
