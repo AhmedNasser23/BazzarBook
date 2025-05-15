d1fdb1a311c587011f24e5abef632b6f1348f71b
ahmad <ahmednasser2330052gmail.com>
2025-05-15 18:02:15 +0300

    Add Identity

--- Bazzar.DataAccess/Data/ApplicationDbContext.cs
+++ Bazzar.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Bazzar.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bazzar.DataAccess.Data
 {
-	public class ApplicationDbContext : DbContext
+	public class ApplicationDbContext : IdentityDbContext
 	{
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 		public DbSet<Category> Categories { get; set; }
@@ -11,6 +12,7 @@ namespace Bazzar.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
+            base.OnModelCreating(modelBuilder);
 			modelBuilder.Entity<Category>().HasData(
 				new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
 				new Category { Id = 2, Name = "Scifi", DisplayOrder = 2 },
